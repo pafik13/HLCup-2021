f62acc49d83dae4b6cf5405d7bf316b2da3b2cdb
pafik13 <re1ax_88@mail.ru>
2021-02-20 23:29:38 +0300

    Second version

--- try-ts/src/index.ts
+++ try-ts/src/index.ts
@@ -38,13 +38,13 @@ type Treasure = {
 import axios, {AxiosInstance} from 'axios';
 import debug from 'debug';
 
-console.debug('start');
+console.debug('start ' + process.env.INSTANCE_ID);
 
 const baseURL = `http://${process.env.ADDRESS}:8000`;
 console.debug('base url: ', baseURL);
 
 process.env.DEBUG = 'client';
-const client = axios.create({baseURL});
+const client = axios.create({baseURL, validateStatus: () => true});
 const logger = debug('client');
 // addLogger(client, logger);
 
@@ -52,85 +52,121 @@ function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const game = async (client: AxiosInstance) => {
-  const wallet: Wallet = {
-    balance: 0,
-    wallet: [],
+class APIClient {
+  private axiosConfigForCash = {
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8',
+    },
   };
-  for (let x = 0; x < 3500; x++) {
-    for (let y = 0; y < 3500; y++) {
-      const area: Area = {
-        posX: x,
-        posY: y,
-        sizeX: 1,
-        sizeY: 1,
-      };
-      let result;
-      try {
-        result = await client.post<Explore>('/explore', area);
-      } catch (error) {
-        logger('explore error %o', error);
-      }
-      if (!result) continue;
+  public client: AxiosInstance;
+  public license?: License;
+  constructor(client: AxiosInstance) {
+    this.client = client;
+  }
 
-      const {data: explore} = result;
-      let depth = 1;
-      let left = explore.amount;
-      let license: License = {
-        digAllowed: 0,
-        digUsed: 0,
-      };
-      while (depth <= 10 && left > 0) {
-        await sleep(10);
-        while (!license.id || license.digUsed >= license.digAllowed) {
-          try {
-            const {data} = await client.post<License>('/licenses', []);
-            license = data;
-          } catch (error) {
-            // logger('licenses error %o', error);
-          }
-        }
-        // logger(license)
-        const dig: Dig = {
-          licenseID: license.id,
+  async post_license(coin: number[]): Promise<License | null> {
+    const result = await this.client.post<License>('/licenses', coin);
+    if (result.status === 200) return result.data;
+    return null;
+  }
+
+  async get_license(): Promise<License[] | null> {
+    const result = await this.client.get<License[]>('/licenses');
+    if (result.status === 200) return result.data;
+    return null;
+  }
+
+  async post_dig(dig: Dig): Promise<Treasure | null> {
+    const result = await this.client.post<string[]>('/dig', dig);
+    if (result.status === 200) return {priority: 0, treasures: result.data};
+    if (result.status === 403 && this.license) delete this.license.id;
+    return null;
+  }
+
+  async post_cash(treasure: string): Promise<number[] | null> {
+    const result = await this.client.post<number[]>(
+      '/cash',
+      JSON.stringify(treasure),
+      this.axiosConfigForCash
+    );
+    if (result.status === 200) return result.data;
+    return null;
+  }
+
+  async post_explore(area: Area): Promise<Explore | null> {
+    const result = await this.client.post<Explore>('/explore', area);
+    if (result.status === 200) {
+      result.data.priority = 0;
+      return result.data;
+    }
+    return null;
+  }
+
+  async get_balance(): Promise<Wallet | null> {
+    const result = await this.client.get<Wallet>('balance');
+    if (result.status === 200) return result.data;
+    return null;
+  }
+
+  async update_license(coins: number[] = []): Promise<void> {
+    const licence = await this.post_license(coins);
+    if (licence) this.license = licence;
+  }
+}
+
+const game = async (client: APIClient) => {
+  // const wallet: Wallet = {
+  //   balance: 0,
+  //   wallet: [],
+  // };
+  const instanceId = Number(process.env.INSTANCE_ID);
+  for (let x = instanceId * 875; x < (instanceId + 1) * 875; x++) {
+    for (let y = instanceId * 875; y < (instanceId + 1) * 875; y++) {
+      try {
+        const area: Area = {
           posX: x,
           posY: y,
-          depth,
+          sizeX: 1,
+          sizeY: 1,
         };
+        const explore = await client.post_explore(area);
+        if (!explore || !explore.amount) continue;
 
-        const treasureWrapper: Treasure = {
-          priority: 0,
-          treasures: [],
-        };
-        try {
-          const {data} = await client.post<string[]>('/dig', dig);
-          treasureWrapper.treasures = data;
-        } catch (error) {
-          // logger('dig error %o', error);
-        }
-        license.digUsed++;
-        depth++;
-        if (treasureWrapper.treasures.length) {
-          for (const treasure of treasureWrapper.treasures) {
-            try {
-              const {data} = await client.post<number[]>('/cash', JSON.stringify(treasure), {
-                headers: {
-                  'Content-Type': 'application/json;charset=UTF-8',
-                },
-              });
-              logger('cash %o', data);
-              for (const cash of data) {
-                wallet.wallet.push(cash);
-              }
-              left--;
-            } catch (error) {
-              logger('cash error %o', error);
+        let depth = 1;
+        let left = explore.amount;
+        while (depth <= 10 && left > 0) {
+          while (
+            !client.license ||
+            !client.license.id ||
+            client.license.digUsed >= client.license.digAllowed
+          ) {
+            await client.update_license();
+          }
+          const dig: Dig = {
+            licenseID: client.license.id,
+            posX: x,
+            posY: y,
+            depth,
+          };
+
+          const treasures = await client.post_dig(dig);
+          client.license.digUsed++;
+          depth++;
+          if (treasures) {
+            for (const treasure of treasures.treasures) {
+              const res = await client.post_cash(treasure);
+              if (res) left--;
             }
           }
         }
+      } catch (error) {
+        logger('global error: %o', error);
+        sleep(100);
       }
     }
   }
 };
 
-game(client);
+const apiClient = new APIClient(client);
+
+game(apiClient);
