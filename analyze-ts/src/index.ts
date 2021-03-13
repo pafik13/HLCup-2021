022da0e1f820629cd50c17c3ff6465a3a27d4535
pafik13 <re1ax_88@mail.ru>
2021-03-14 00:23:30 +0300

    Add license, dig and cash + call stats

--- analyze-ts/src/index.ts
+++ analyze-ts/src/index.ts
@@ -19,15 +19,23 @@ type GlobalStats = {
   total: number[];
 };
 
+type License = {
+  id: number;
+  digAllowed: number;
+  digUsed: number;
+};
+
 import {request, RequestOptions} from 'http';
 import {performance} from 'perf_hooks';
 import * as ss from 'simple-statistics';
+import {inspect} from 'util';
 
 const baseURL = `http://${process.env.ADDRESS}:8000`;
 const GLOBAL_OFFSET_X = Number(process.env.GLOBAL_OFFSET_X) || 0;
 const GLOBAL_OFFSET_Y = Number(process.env.GLOBAL_OFFSET_Y) || 0;
 const EXPLORE_CONCURRENCY = Number(process.env.EXPLORE_CONCURRENCY) || 1;
 const EXPLORE_SIZE = Number(process.env.EXPLORE_SIZE) || 16;
+const PRINT_STATS_TIME = Number(process.env.PRINT_STATS_TIME) || 60000;
 console.debug(
   'envs: ',
   baseURL,
@@ -46,6 +54,43 @@ const globalStats: Record<string, GlobalStats> = {};
 const exploreStats = {
   tries: 0,
   amount: 0,
+};
+
+class CallStats {
+  public count: Record<number, number> = {};
+  public time: Record<number, number> = {};
+
+  setTime = (status: number, time: number): void => {
+    if (this.time[status]) {
+      this.time[status] += time;
+    } else {
+      this.time[status] = time;
+    }
+    if (this.count[status]) {
+      this.count[status]++;
+    } else {
+      this.count[status] = 1;
+    }
+  };
+
+  [inspect.custom]() {
+    let countStr = '';
+    for (const [key, value] of Object.entries(this.count)) {
+      countStr += `${key}: ${value}; `;
+    }
+    let timeStr = '';
+    for (const [key, value] of Object.entries(this.time)) {
+      timeStr += `${key}: ${value.toFixed(2)}; `;
+    }
+    return `[${countStr}; ${timeStr}]`;
+  }
+}
+
+const callStats = {
+  dig: new CallStats(),
+  cash: new CallStats(),
+  licenseFree: new CallStats(),
+  licensePaid: new CallStats(),
 };
 
 const summary = function (input: number[]) {
@@ -67,6 +112,9 @@ const writeStats = function () {
     for (const [key, values] of Object.entries(stats)) {
       console.debug(`${status}-${key}: ${values.length} ${summary(values)}`);
     }
+  }
+  for (const [key, stats] of Object.entries(callStats)) {
+    console.debug(`${key}: ${inspect(stats)}`);
   }
 };
 
@@ -226,15 +274,271 @@ const post_explore = async function (area: Area): Promise<Explore | null> {
       });
     });
 
-    req.write(JSON.stringify(area));
+    req.write(data);
     req.end();
   });
 };
 
+const wallet: number[] = [];
+let license: License | null = null;
+const update_license = async function (): Promise<void> {
+  return new Promise(resolve => {
+    const coins: number[] = [];
+    const data = JSON.stringify(coins);
 
+    const timings = {
+      startAt: performance.now(),
+      dnsLookupAt: 0,
+      tcpConnectionAt: 0,
+      firstByteAt: 0,
+      endAt: 0,
+    };
+
+    const options: RequestOptions = {
+      hostname: process.env.ADDRESS,
+      port: 8000,
+      path: '/licenses',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+      },
+    };
+
+    const chunks: Buffer[] = [];
+    const req = request(options, res => {
+      // res.once('readable', () => {
+      //   timings.firstByteAt = performance.now();
+      // });
+      res.on('data', (chunk: Buffer) => {
+        chunks.push(chunk);
+      });
+      res.on('end', () => {
+        // timings.endAt = performance.now();
+        const status = res.statusCode || -1;
+        if (status === 200) {
+          license = JSON.parse(Buffer.concat(chunks).toString()) as License;
+        }
+        if (coins.length) {
+          callStats.licensePaid.setTime(
+            status,
+            performance.now() - timings.startAt
+          );
+        } else {
+          callStats.licenseFree.setTime(
+            status,
+            performance.now() - timings.startAt
+          );
+        }
+        resolve();
+        // const stats = globalStats[status];
+        // if (stats) {
+        //   stats.dnsLookup.push(timings.dnsLookupAt - timings.startAt);
+        //   stats.tcpConnection.push(
+        //     (timings.dnsLookupAt || timings.tcpConnectionAt) - timings.startAt
+        //   );
+        //   stats.firstByte.push(timings.firstByteAt - timings.tcpConnectionAt);
+        //   stats.contentTransfer.push(timings.endAt - timings.firstByteAt);
+        //   stats.total.push(timings.endAt - timings.startAt);
+        // } else {
+        //   globalStats[status] = {
+        //     dnsLookup: [timings.dnsLookupAt - timings.startAt],
+        //     tcpConnection: [
+        //       (timings.dnsLookupAt || timings.tcpConnectionAt) -
+        //         timings.startAt,
+        //     ],
+        //     firstByte: [timings.firstByteAt - timings.tcpConnectionAt],
+        //     contentTransfer: [timings.endAt - timings.firstByteAt],
+        //     total: [timings.endAt - timings.startAt],
+        //   };
+        // }
+      });
+    });
+    // req.on('socket', socket => {
+    //   socket.on('lookup', () => {
+    //     timings.dnsLookupAt = performance.now();
+    //   });
+    //   socket.on('connect', () => {
+    //     timings.tcpConnectionAt = performance.now();
+    //   });
+    // });
+
+    req.write(data);
+    req.end();
+  });
+};
+
+type Dig = {
+  licenseID: number;
+  posX: number;
+  posY: number;
+  depth: number;
+};
+
+const post_dig = async function (dig: Dig): Promise<string[] | null> {
+  return new Promise(resolve => {
+    const data = JSON.stringify(dig);
+
+    const timings = {
+      startAt: performance.now(),
+      dnsLookupAt: 0,
+      tcpConnectionAt: 0,
+      firstByteAt: 0,
+      endAt: 0,
+    };
+
+    const options: RequestOptions = {
+      hostname: process.env.ADDRESS,
+      port: 8000,
+      path: '/dig',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+      },
+    };
+
+    const chunks: Buffer[] = [];
+    const req = request(options, res => {
+      // res.once('readable', () => {
+      //   timings.firstByteAt = performance.now();
+      // });
+      res.on('data', (chunk: Buffer) => {
+        chunks.push(chunk);
+      });
+      res.on('end', () => {
+        // timings.endAt = performance.now();
+        const status = res.statusCode || -1;
+        if (status === 200) {
+          resolve(JSON.parse(Buffer.concat(chunks).toString()) as string[]);
+        } 
+        // else {
+        //   console.debug(JSON.parse(Buffer.concat(chunks).toString()));
+        // }
+        callStats.dig.setTime(status, performance.now() - timings.startAt);
+        resolve(null);
+        // const stats = globalStats[status];
+        // if (stats) {
+        //   stats.dnsLookup.push(timings.dnsLookupAt - timings.startAt);
+        //   stats.tcpConnection.push(
+        //     (timings.dnsLookupAt || timings.tcpConnectionAt) - timings.startAt
+        //   );
+        //   stats.firstByte.push(timings.firstByteAt - timings.tcpConnectionAt);
+        //   stats.contentTransfer.push(timings.endAt - timings.firstByteAt);
+        //   stats.total.push(timings.endAt - timings.startAt);
+        // } else {
+        //   globalStats[status] = {
+        //     dnsLookup: [timings.dnsLookupAt - timings.startAt],
+        //     tcpConnection: [
+        //       (timings.dnsLookupAt || timings.tcpConnectionAt) -
+        //         timings.startAt,
+        //     ],
+        //     firstByte: [timings.firstByteAt - timings.tcpConnectionAt],
+        //     contentTransfer: [timings.endAt - timings.firstByteAt],
+        //     total: [timings.endAt - timings.startAt],
+        //   };
+        // }
+      });
+    });
+    // req.on('socket', socket => {
+    //   socket.on('lookup', () => {
+    //     timings.dnsLookupAt = performance.now();
+    //   });
+    //   socket.on('connect', () => {
+    //     timings.tcpConnectionAt = performance.now();
+    //   });
+    // });
+
+    req.write(data);
+    req.end();
+  });
+};
+
+const post_cash = async function (treasure: string): Promise<void> {
+  return new Promise(resolve => {
+    const data = JSON.stringify(treasure);
+
+    const timings = {
+      startAt: performance.now(),
+      dnsLookupAt: 0,
+      tcpConnectionAt: 0,
+      firstByteAt: 0,
+      endAt: 0,
+    };
+
+    const options: RequestOptions = {
+      hostname: process.env.ADDRESS,
+      port: 8000,
+      path: '/cash',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+      },
+    };
+
+    const chunks: Buffer[] = [];
+    const req = request(options, res => {
+      // res.once('readable', () => {
+      //   timings.firstByteAt = performance.now();
+      // });
+      res.on('data', (chunk: Buffer) => {
+        chunks.push(chunk);
+      });
+      res.on('end', () => {
+        // timings.endAt = performance.now();
+        const status = res.statusCode || -1;
+        if (status === 200) {
+          for (const coin of JSON.parse(
+            Buffer.concat(chunks).toString()
+          ) as number[]) {
+            wallet.push(coin);
+          }
+        }
+        // else {
+        //   console.debug(Buffer.concat(chunks).toString())
+        // }
+        callStats.cash.setTime(status, performance.now() - timings.startAt);
+        resolve();
+        // const stats = globalStats[status];
+        // if (stats) {
+        //   stats.dnsLookup.push(timings.dnsLookupAt - timings.startAt);
+        //   stats.tcpConnection.push(
+        //     (timings.dnsLookupAt || timings.tcpConnectionAt) - timings.startAt
+        //   );
+        //   stats.firstByte.push(timings.firstByteAt - timings.tcpConnectionAt);
+        //   stats.contentTransfer.push(timings.endAt - timings.firstByteAt);
+        //   stats.total.push(timings.endAt - timings.startAt);
+        // } else {
+        //   globalStats[status] = {
+        //     dnsLookup: [timings.dnsLookupAt - timings.startAt],
+        //     tcpConnection: [
+        //       (timings.dnsLookupAt || timings.tcpConnectionAt) -
+        //         timings.startAt,
+        //     ],
+        //     firstByte: [timings.firstByteAt - timings.tcpConnectionAt],
+        //     contentTransfer: [timings.endAt - timings.firstByteAt],
+        //     total: [timings.endAt - timings.startAt],
+        //   };
+        // }
+      });
+    });
+    // req.on('socket', socket => {
+    //   socket.on('lookup', () => {
+    //     timings.dnsLookupAt = performance.now();
+    //   });
+    //   socket.on('connect', () => {
+    //     timings.tcpConnectionAt = performance.now();
+    //   });
+    // });
+
+    req.write(data);
+    req.end();
+  });
+};
 
 const start = async () => {
-  const statsInterval = setInterval(() => writeStats(), 60000);
+  const statsInterval = setInterval(() => writeStats(), PRINT_STATS_TIME);
   statsInterval.unref();
 
   const instanceId = Number(process.env.INSTANCE_ID);
@@ -281,6 +585,28 @@ const start = async () => {
           for (const result of results) {
             if (result && result.amount) {
               exploreStats.amount++;
+              let left = result.amount;
+              let depth = 1;
+              while (left && depth < 10) {
+                while (!license || license.digUsed >= license.digAllowed) {
+                  await update_license();
+                }
+                const dig: Dig = {
+                  depth,
+                  posX: result.area.posX,
+                  posY: result.area.posY,
+                  licenseID: license.id,
+                };
+                const treasures = await post_dig(dig);
+                if (treasures) {
+                  left--;
+                  for (const treasure of treasures) {
+                    await post_cash(treasure);
+                  }
+                }
+                license.digUsed++;
+                depth++;
+              }
             }
           }
           tasks.length = 0;
